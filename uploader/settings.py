b06372a9582546047eabfb7ce6f0ce5c1b18394d
Moeid <ostadsisq@gmail.com>
2024-08-16 12:44:14 +0330

    using postgresql instead sqlite

--- uploader/settings.py
+++ uploader/settings.py
@@ -77,15 +77,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'uploader.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE'  : 'django.db.backends.postgresql', 
+    'NAME'    : 'uploader',
+    'USER'    : 'postgres',
+    'PASSWORD': 'root',
+    'HOST'    : 'localhost',
+    'PORT'    : '5432',
+    }, 
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
